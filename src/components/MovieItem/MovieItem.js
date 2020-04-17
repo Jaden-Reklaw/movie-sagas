c8a5f14f2c2aa30885f89b6da44381194e20a18a
Jordan Walker <jwalker117@gmail.com>
2020-04-17 15:04:07 -0500

    Worked on css for MovieItem.js

--- src/components/MovieItem/MovieItem.js
+++ src/components/MovieItem/MovieItem.js
@@ -6,9 +6,16 @@ import './MovieItem.css';
 class MovieItem extends Component {
   // Renders the entire app on the DOM
   render() {
+    const movie = this.props.movie;
     return (
-      <div>
-        {JSON.stringify(this.props.movie)}
+      <div className='movie-container'>
+        <div className='image-pic'>
+          <img src={movie.poster} alt={movie.description}/>
+        </div>
+        <div>
+          <h4>{movie.title}</h4>
+          <p>{movie.description}</p>
+        </div>
       </div>
     );
   }
